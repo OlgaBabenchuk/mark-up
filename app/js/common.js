dde94ec38a0fb41c6f5e3def7630ba13dd011e84
USERPC\User <olgababenchuk2@gmail.com>
2017-09-14 14:16:13 +0300

    change jQury to pure JS

--- app/js/common.js
+++ app/js/common.js
@@ -1,27 +1,35 @@
-$(document).ready(function() {
+window.onload = function() {
+
+  var menu = document.getElementsByClassName('mob-trigger')[0];
+  var body = document.getElementsByTagName('BODY')[0];
+  var item = document.getElementsByClassName('profiles__item');
+  var pagraph = document.getElementsByClassName('text');
 
   /*   toggle sidebar menu   */
+  function toggleMenu(){
+    body.classList.toggle('menu-open');
+    this.classList.toggle('mob-trigger-close');
+  }
 
-  $(".mob-trigger").on("click", function() {
-    $("body").toggleClass('menu-open');
-    $(this).toggleClass('mob-trigger-close');
-  });
+  menu.addEventListener("click", toggleMenu);
 
   /*  settings for select block item  */
+  function setItem(){
+    this.classList.toggle('profiles__item--selected');
+  }
 
-  $(".profiles__item").on("click", function() {    
-    $(this).toggleClass('profiles__item--selected');
+  [].forEach.call(item, function(e) {
+    e.addEventListener("click", setItem);
   });
 
   /*  adding "..." to the long text */
-  
-  $(".text p").each(function(){
-  var review_full = $(this).html();
+  [].forEach.call(pagraph, function(e) {
+    var review_full = e.innerHTML;
     var review = review_full;
     if( review.length > 150 )
     {
       review = review.substring(0, 150);
-      $(this).html( review + ' ...' );
+      e.innerHTML = review + ' ...';
     }
   });
-});
+}
